b7b171d9fe323ddcf62ada1f6beb913615cbcc99
mfreema1 <mfreema1@stevens.edu>
2018-09-13 11:58:07 -0400

    Update project structure

---
+++ index.py
@@ -0,0 +1,13 @@
+"""
+Entry point for GEDCOM parsing application.
+
+@author: Mark Freeman, Michael Ameer, Besnik Balaj, Kipsy Quevada
+"""
+
+from modules import example
+
+def main():
+    example.greet('Mark')
+
+if __name__ == '__main__':
+    main()
---
+++ modules/example.py
@@ -0,0 +1,11 @@
+"""
+Example module.
+
+@author: Mark Freeman
+"""
+
+def _say_hi(str):
+    print('Hello {}!'.format(str))
+
+def greet(str):
+    _say_hi(str)
