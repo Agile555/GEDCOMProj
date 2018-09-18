0c75b9bfadb5fcc2c4f45f220ea9fe61536db815
mfreema1 <mfreema1@stevens.edu>
2018-09-18 18:34:51 -0400

    Update DB and include DB reset script

---
+++ reset_db.py
@@ -0,0 +1,13 @@
+#Mark Freeman
+
+#just a utility to reset the database if it gets freaky
+import sqlite3
+
+conn = sqlite3.connect('megatron.db')
+c = conn.cursor()
+
+c.execute("DROP TABLE IF EXISTS INDI")
+c.execute("DROP TABLE IF EXISTS FAM")
+
+c.execute("CREATE TABLE INDI(ID TEXT PRIMARY KEY, Name TEXT, Gender TEXT, Birthday TEXT, Age INTEGER, Alive TEXT, Death TEXT, Child TEXT, Spouse TEXT)")
+c.execute("CREATE TABLE FAM(ID TEXT PRIMARY KEY, Married TEXT, Divorced TEXT, 'Husband ID' TEXT, 'Husband Name' TEXT, 'Wife ID' TEXT, 'Wife Name' TEXT, Children TEXT)")
