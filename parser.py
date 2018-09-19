a426d9bd23fbe7a8ca82b7696994b35980fd3ad7
mfreema1 <mfreema1@stevens.edu>
2018-09-18 21:25:06 -0400

    Finish multi-spouse and multi-child feature

--- parser.py
+++ parser.py
@@ -119,63 +119,10 @@ i = {
     'FAM': dict(zip(famTableTags, range(len(famTableTags))))
 }
 
-stack = []
-cursor = []
+stack = [] #controls parent-child relationships among tags
+cursor = [] #modifiable buffer to hold data to be inserted into DB
 
-def adjustEntries(type): #after filling in direct related tags, fill secondary related
-    global cursor #i am a truly lost man
-    if(type == 'INDI'):
-        if(exists(cursor[i[type]['Birthday']])): #we were given a birth date, must fill in age
-            if(exists(cursor[i[type]['Death']])): #age will diff between age and death
-                cursor[i[type]['Age']] = (cursor[i[type]['Death']] - cursor[i[type]['Birthday']]).days // 365 #TODO: technically not right with leap years
-            else: #didn't die, age is diff between birth and now
-                cursor[i[type]['Age']] = (datetime.date.today() - cursor[i[type]['Birthday']]).days // 365
-        if(exists(cursor[i[type]['Death']])):
-            cursor[i[type]['Alive']] = 'N'
-        else:
-            cursor[i[type]['Alive']] = 'Y'
-    #TODO add query support
-    elif(type == 'FAM'):
-        addSpouseNames(cursor[i[type]['Husband ID']], cursor[i[type]['Wife ID']])
-
-def append(index, str):
-    global cursor
-    if(exists(cursor[index])):
-        cursor[index] = [cursor[index], str]
-    else:
-        cursor[index] = str
-
-def exists(str):
-    return (str != 'NA')
-
-#TODO: name-ify
-def addSpouseNames(husb, wife):
-    global cursor
-    val = searchDB('INDI', 'Name', 'ID', husb).fetchone()
-    if(val):
-        cursor[4] = val[0] #it's a cursor, need to subscript
-    val = searchDB('INDI', 'Name', 'ID', wife).fetchone()
-    if(val):
-        cursor[6] = val[0]
-
-def insertIntoDB(table, lst):
-    if(table == 'INDI'):
-        return c.execute('INSERT INTO INDI VALUES (?,?,?,?,?,?,?,?,?)', lst)
-    if(table == 'FAM'):
-        return c.execute('INSERT INTO FAM VALUES (?,?,?,?,?,?,?,?)', lst)
-    
-def searchDB(table, column, name, value):
-    return c.execute("SELECT {} FROM {} WHERE {}='{}'".format(column, table, name, value)) #for some reason SQLite wants them in quotes
-
-def isBackWardTag(str):
-    if(d.get(str) and d.get(str).get('backwards')):
-        return True
-    return False
-
-def isValidLevel(tagName, level):
-    if(d.get(tagName) and d.get(tagName).get('level') == level):
-        return True
-    return False
+#################### Stack Methods #####################
 
 def isProperChild(tagName):
     for tag in stack: #search the stack for the parent to deem if it can go on
@@ -190,14 +137,75 @@ def repairStack(tagName):
     elif(d.get(tagName).get('level') == '1'): #if it's level 1, replace second to last element and remove all others
         stack = [stack[0], tagName]
 
+def adjustEntries(type): #after filling in direct related tags, fill secondary related
+    if(type == 'INDI'):
+        if(exists(cursor[i[type]['Birthday']])): #we were given a birth date, must fill in age
+            if(exists(cursor[i[type]['Death']])): #age will diff between age and death
+                append(i[type]['Age'], (cursor[i[type]['Death']] - cursor[i[type]['Birthday']]).days // 365) #TODO: technically not right with leap years
+            else: #didn't die, age is diff between birth and now
+                append(i[type]['Age'], (datetime.date.today() - cursor[i[type]['Birthday']]).days // 365)
+        if(exists(cursor[i[type]['Death']])):
+            append(i[type]['Alive'], 'N')
+        else:
+            append(i[type]['Alive'], 'Y')
+    elif(type == 'FAM'):
+        addSpouseNames(cursor[i[type]['Husband ID']], cursor[i[type]['Wife ID']])
+
+#################### Database Methods ##################
+
+def searchDB(table, column, name, value):
+    return c.execute("SELECT {} FROM {} WHERE {}='{}'".format(column, table, name, value)) #for some reason SQLite wants them in quotes
+
+def insertIntoDB(table, lst):
+    if(table == 'INDI'):
+        return c.execute('INSERT INTO INDI VALUES (?,?,?,?,?,?,?,?,?)', lst)
+    if(table == 'FAM'):
+        return c.execute('INSERT INTO FAM VALUES (?,?,?,?,?,?,?,?)', lst)
+
+#################### Cursor Methods ####################
+
+def emptyCursor(num_slots): #sometimes global is a necessary evil
+    global cursor
+    cursor = ["NA"] * num_slots
+
+def append(index, str):
+    global cursor
+    if(exists(cursor[index])):
+        cursor[index] = ",".join([cursor[index], str])
+    else:
+        cursor[index] = str
+
+def addSpouseNames(husb, wife):
+    val = searchDB('INDI', 'Name', 'ID', husb).fetchone()
+    if(val):
+        append(i['FAM']['Husband Name'], val[0]) #it's a cursor, need to subscript
+    val = searchDB('INDI', 'Name', 'ID', wife).fetchone()
+    if(val):
+        append(i['FAM']['Wife Name'], val[0])
+
+def exists(str):
+    return (str != 'NA')
+
+#################### Tag Methods #######################
+
+def isBackWardTag(str):
+    if(d.get(str) and d.get(str).get('backwards')):
+        return True
+    return False
+    
+def isValidLevel(tagName, level):
+    if(d.get(tagName) and d.get(tagName).get('level') == level):
+        return True
+    return False
+
+#################### Main Methods ######################
+
 def createMessage(tagName, level, letter, args):
     return '<--' + level + '|' + tagName + '|' + letter + '|' + " ".join(args)
 
-#TODO: name-ify these cursor calls to make the code more idiomatic
 def process(line): #goal is to make sure that it is at its own valid level AND preceding a parent
     arr = line.split(" ")
     level, tagName, args = arr[0], None, [] #we know the level always
-    global cursor
 
     #phase 1: assign variables
     if(len(arr) == 2):
@@ -217,26 +225,25 @@ def process(line): #goal is to make sure that it is at its own valid level AND p
             if(isBackWardTag(tagName)): #only backward tags constitute a new entry
                 if(cursor): #if one is already populated, go and insert it
                     adjustEntries(stack[0])
-                    print(cursor)
                     insertIntoDB(stack[0], cursor)
                 repairStack(tagName)
-                cursor = ["NA"] * d[tagName]['number_of_attributes'] #fancy
-                
-                cursor[i[tagName][d[tagName]['parallel']]] = " ".join(args) #insert id
+                emptyCursor(d[tagName]['number_of_attributes']) #empty
+                append(i[tagName][d[tagName]['parallel']], " ".join(args)) #insert id
                 #now we have a cursor with the id filled that we can use
             return createMessage(tagName, level, 'Y', args)
         elif(level == '1'):
             if(isProperChild(tagName)):
                 repairStack(tagName)
-                cursor[i[stack[0]][d[tagName]['parallel']]] = " ".join(args)
+                if(args): #we don't want to overwrite the NA if we have no args to put there anyways
+                    append(i[stack[0]][d[tagName]['parallel']], " ".join(args))
                 return createMessage(tagName, level, 'Y', args)
             else:
                 return createMessage(tagName, level, 'N', args)
         else: #level is 2, so far only date can have this so I'll throw a strip time right in and not check
             if(isProperChild(tagName)):
                 repairStack(tagName)
-                parent = stack[-1] #the last element on the stack is the immediate parent we need to modify
-                cursor[i[stack[0]][d[parent]['parallel']]] = datetime.datetime.strptime(" ".join(args), '%d %b %Y').date()
+                parent = stack[-1] #the last element on the stack is the immediate parent we need to modify with date
+                append(i[stack[0]][d[parent]['parallel']], datetime.datetime.strptime(" ".join(args), '%d %b %Y').date())
                 return createMessage(tagName, level, 'Y', args)
             else:
                 return createMessage(tagName, level, 'N', args)
@@ -244,8 +251,8 @@ def process(line): #goal is to make sure that it is at its own valid level AND p
         return createMessage(tagName, level, 'N', args) #it wasn't a tag or it wasn't a valid level for that tag
 
 def main():
-    #file_name = input('Please enter the name of the file you wish to validate: ')
-    file_name = 'tests/family.ged'
+    file_name = input('Please enter the name of the file you wish to validate: ')
+    #file_name = 'tests/family.ged' or 'test/proj02test.ged' for debugging
     with open(file_name, 'r') as f:
 
         for line in f:
