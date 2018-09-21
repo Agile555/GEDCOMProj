6895f64e0d27ba0aaa173f80c24e21be61255be0
mfreema1 <mfreema1@stevens.edu>
2018-09-21 11:50:49 -0400

    Update and enforce style guide

--- parser.py
+++ parser.py
@@ -1,4 +1,8 @@
-#Mark Freeman, SSW555
+"""
+Main parser module that populates an SQLite database from a .ged input file.  Please note that this
+parser does not support level 2 tags apart from the DATE tag.
+@author: Mark Freeman
+"""
 
 from prettytable import from_db_cursor
 import datetime
@@ -112,11 +116,11 @@ d = { #not all tags have parallels, but all headers come from one or more tags
 }
 
 #inflate a dictionary to hold the tags and their position in the table
-indiTableTags = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
-famTableTags = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
+indi_table_tags = ['ID', 'Name', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse']
+fam_table_tags = ['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children']
 i = {
-    'INDI': dict(zip(indiTableTags, range(len(indiTableTags)))),
-    'FAM': dict(zip(famTableTags, range(len(famTableTags))))
+    'INDI': dict(zip(indi_table_tags, range(len(indi_table_tags)))),
+    'FAM': dict(zip(fam_table_tags, range(len(fam_table_tags))))
 }
 
 stack = [] #controls parent-child relationships among tags
@@ -124,20 +128,61 @@ cursor = [] #modifiable buffer to hold data to be inserted into DB
 
 #################### Stack Methods #####################
 
-def isProperChild(tagName):
+def is_proper_child(tag_name):
+    """
+    Determines if a tag is allowed in the place it is trying to be placed.  All tags below
+    level 0 must be a proper child of the tag above them.
+
+    Args:
+        tag_name (string): the name of the tag we are 
+            trying to insert
+
+    Returns:
+        (bool): the tag can be inserted here    
+    """
     for tag in stack: #search the stack for the parent to deem if it can go on
-        if(tag in d.get(tagName).get('parents')):
+        if(tag in d.get(tag_name).get('parents')):
             return True
     return False
 
-def repairStack(tagName):
-    global stack #forgive me
-    if(d.get(tagName).get('level') == '0'): #if it's level 0, empty the stack and replace bottom with new tag
-        stack = [tagName]
-    elif(d.get(tagName).get('level') == '1'): #if it's level 1, replace second to last element and remove all others
-        stack = [stack[0], tagName]
+def repair_stack(tag_name):
+    """
+    Fixes the stack as we insert objects.  Level 0 items will clear the stack completely, 
+    while all others will clear the stack down to their level.
 
-def adjustEntries(type): #after filling in direct related tags, fill secondary related
+    Args:
+        tag_name (string): the name of the tag to place 
+            on the stack
+    """
+    global stack #forgive me for I have sinned
+    if(d.get(tag_name).get('level') == '0'): #if it's level 0, empty the stack and replace bottom with new tag
+        stack = [tag_name]
+    elif(d.get(tag_name).get('level') == '1'): #if it's level 1, replace second to last element and remove all others
+        stack = [stack[0], tag_name]
+
+def adjust_entries(type): #after filling in direct related tags, fill secondary related
+    """
+    Adjusts the cursor we build to include secondary entries, which are generated from
+    direct entires.
+
+    Definitions:
+        Direct Entry: An entry in the cursor or table 
+            which does not come directly from a tag.  
+            For example, the `Name` attribute is 
+            direct because it is populated directly
+            by the NAME tag.
+        Secondary Entry: A tag which does not correlate 
+            directly to an entry in the cursor or table.  
+            For example, the `Age` attribute is secondary 
+            because it results from some combination of the
+            `Birthday` and `Death` entries, which in turn 
+            come directly from the BIRT and DEAT tags.
+            
+    Args:
+        type (string): Either `INDI` or `FAM`, which 
+            denotes the cursor format that is currently 
+            being used.
+    """
     if(type == 'INDI'):
         if(exists(cursor[i[type]['Birthday']])): #we were given a birth date, must fill in age
             if(exists(cursor[i[type]['Death']])): #age will diff between age and death
@@ -149,95 +194,182 @@ def adjustEntries(type): #after filling in direct related tags, fill secondary r
         else:
             append(i[type]['Alive'], 'Y')
     elif(type == 'FAM'):
-        addSpouseNames(cursor[i[type]['Husband ID']], cursor[i[type]['Wife ID']])
+        add_spouse_names(cursor[i[type]['Husband ID']], cursor[i[type]['Wife ID']])
 
 #################### Database Methods ##################
 
-def searchDB(table, column, name, value):
+
+def search_db(table, column, name, value):
+    """
+    SQLite wrapper to expose a simple selection query as a function.
+
+    Args:
+        table (string): the name of the table to search
+        column (string): the column to return values from
+        name (string): name of the value to match on
+        value (string): value to match with
+
+    Returns:
+        (cursor): cursor representing all rows that 
+            matched the search  
+    """
     return c.execute("SELECT {} FROM {} WHERE {}='{}'".format(column, table, name, value)) #for some reason SQLite wants them in quotes
 
-def insertIntoDB(table, lst):
+def insert_into_db(table, lst):
+    """
+    SQLite wrapper to expose an insertion query as a function.
+
+    Args:
+        table (string): the name of the table to insert into
+        lst (list): a list of values to insert into 
+            the database
+    """
     if(table == 'INDI'):
-        return c.execute('INSERT INTO INDI VALUES (?,?,?,?,?,?,?,?,?)', lst)
+        c.execute('INSERT INTO INDI VALUES (?,?,?,?,?,?,?,?,?)', lst) #value schemes are different for different tables
     if(table == 'FAM'):
-        return c.execute('INSERT INTO FAM VALUES (?,?,?,?,?,?,?,?)', lst)
+        c.execute('INSERT INTO FAM VALUES (?,?,?,?,?,?,?,?)', lst)
 
 #################### Cursor Methods ####################
 
-def emptyCursor(num_slots): #sometimes global is a necessary evil
+def empty_cursor(num_slots): #sometimes global is a necessary evil
+    """
+    Empties and repopulates the cursor with an appropriate number of slots.  Generally called after an insertion.
+
+    Args:
+        num_slots (int): the number of "NA" slots to
+            inflate the cursor with
+    """
     global cursor
     cursor = ["NA"] * num_slots
 
 def append(index, str):
+    """
+    Appends an item to the cursor.  If the item already exists in the cursor, it forms a comma separated string of
+    the values and inserts that.
+
+    Args:
+        index (int): the index of the cursor we are targeting
+
+    Returns:
+        str (string): the value to place at that index
+    """
     global cursor
     if(exists(cursor[index])):
         cursor[index] = ",".join([cursor[index], str])
     else:
         cursor[index] = str
 
-def addSpouseNames(husb, wife):
-    val = searchDB('INDI', 'Name', 'ID', husb).fetchone()
+def add_spouse_names(husb, wife):
+    """
+    Retrieve the names of the husband and wife of a family using their IDs and append their name to the cursor.
+
+    Args:
+        husb (string): id of the husband
+        wife (string): id of the wife
+    """
+    val = search_db('INDI', 'Name', 'ID', husb).fetchone()
     if(val):
         append(i['FAM']['Husband Name'], val[0]) #it's a cursor, need to subscript
-    val = searchDB('INDI', 'Name', 'ID', wife).fetchone()
+    val = search_db('INDI', 'Name', 'ID', wife).fetchone()
     if(val):
         append(i['FAM']['Wife Name'], val[0])
 
 def exists(str):
+    """
+    Checks to see whether the value is populated in the cursor
+
+    Args:
+        str (string): the value in the cursor
+
+    Returns:
+        (bool): the value is not null
+    """
     return (str != 'NA')
 
 #################### Tag Methods #######################
 
-def isBackWardTag(str):
+def is_backward_tag(str):
+    """
+    Checks to see if a tag is one of the backwards tags in GEDCOM, namely INDI and FAM.
+
+    Args:
+        str (string): the name of the tag to chck
+
+    Returns:
+        (bool): the tag is a backwards tag
+    """
     if(d.get(str) and d.get(str).get('backwards')):
         return True
     return False
     
-def isValidLevel(tagName, level):
-    if(d.get(tagName) and d.get(tagName).get('level') == level):
+def is_valid_level(tag_name, level):
+    """
+    Checks to see if the tag is allowed at that level.  For instance, a DATE can never be
+    applied at level 0 or 1, only 2.  This does not involve the stack.
+
+    Args:
+        tag_name (string): the name of the tag to check
+        level (string): the attempted level of the tag
+
+    Returns:
+        (bool): the tag can be used at that level    
+    """
+    if(d.get(tag_name) and d.get(tag_name).get('level') == level):
         return True
     return False
 
 #################### Main Methods ######################
 
 def process(line): #goal is to make sure that it is at its own valid level AND preceding a parent
+    """
+    Main function of the parser.  Take in a line from a file as text and inspect it to gather
+    information as to what it is trying to do.  Check if that is allowed, and if it is, go ahead
+    and toss it into the database.  If not, don't bother.
+
+    Args:
+        line (string): one line of the GEDCOM file
+    """
     arr = line.split(" ")
-    level, tagName, args = arr[0], None, [] #we know the level always
+    level, tag_name, args = arr[0], None, [] #we know the level always
 
     #phase 1: assign variables
     if(len(arr) == 2):
-        tagName = arr[1]
+        tag_name = arr[1]
     elif(len(arr) > 2): #we know we have three, now check if it's backwards
-        if(isBackWardTag(arr[1])): #if there's a backward tag in the wrong spot, don't bother
-            tagName, args = arr[1], arr[2:]
-        if(isBackWardTag(arr[2])):
-            tagName, args = arr[2], [arr[1]]
+        if(is_backward_tag(arr[1])): #if there's a backward tag in the wrong spot, don't bother
+            tag_name, args = arr[1], arr[2:]
+        if(is_backward_tag(arr[2])):
+            tag_name, args = arr[2], [arr[1]]
         else:
-            tagName, args = arr[1], arr[2:]
+            tag_name, args = arr[1], arr[2:]
 
     #phase 2: add rows based on what was assigned, soon won't need to print lines
-    if(isValidLevel(tagName, level)): #we have the tagName and level, assure that they are compatible
+    if(is_valid_level(tag_name, level)): #we have the tag_name and level, assure that they are compatible
         if(level == '0'):
-            if(isBackWardTag(tagName)): #only backward tags constitute a new entry
+            if(is_backward_tag(tag_name)): #only backward tags constitute a new entry
                 if(cursor): #if one is already populated, go and insert it
-                    adjustEntries(stack[0])
-                    insertIntoDB(stack[0], cursor)
-                repairStack(tagName)
-                emptyCursor(d[tagName]['number_of_attributes']) #empty
-                append(i[tagName][d[tagName]['parallel']], " ".join(args)) #insert id
+                    adjust_entries(stack[0])
+                    insert_into_db(stack[0], cursor)
+                repair_stack(tag_name)
+                empty_cursor(d[tag_name]['number_of_attributes']) #empty
+                append(i[tag_name][d[tag_name]['parallel']], " ".join(args)) #insert id
                 #now we have a cursor with the id filled that we can use
         elif(level == '1'):
-            if(isProperChild(tagName)):
-                repairStack(tagName)
+            if(is_proper_child(tag_name)):
+                repair_stack(tag_name)
                 if(args): #we don't want to overwrite the NA if we have no args to put there anyways
-                    append(i[stack[0]][d[tagName]['parallel']], " ".join(args))
+                    append(i[stack[0]][d[tag_name]['parallel']], " ".join(args))
         else: #level is 2, so far only date can have this so I'll throw a strip time right in and not check
-            if(isProperChild(tagName)):
-                repairStack(tagName)
+            if(is_proper_child(tag_name)):
+                repair_stack(tag_name)
                 parent = stack[-1] #the last element on the stack is the immediate parent we need to modify with date
                 append(i[stack[0]][d[parent]['parallel']], datetime.datetime.strptime(" ".join(args), '%d %b %Y').date())
 
 def main():
+    """
+    Read in the GEDCOM file entered by the user and process each of them.  After the database
+    is populated, show the contents of the tables.
+    """
     file_name = input('Please enter the name of the file you wish to validate: ')
     #file_name = 'tests/family.ged' or 'test/proj02test.ged' for debugging
     with open(file_name, 'r') as f:
@@ -246,8 +378,8 @@ def main():
             line = line.strip('\n') #take off the newline
             process(line)
 
-        adjustEntries(stack[0])
-        insertIntoDB(stack[0], cursor)
+        adjust_entries(stack[0])
+        insert_into_db(stack[0], cursor)
 
         #go grab the sql tables
         print(from_db_cursor(c.execute('SELECT * FROM INDI ORDER BY ID ASC')))
