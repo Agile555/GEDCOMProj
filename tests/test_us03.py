f07f9ceeea1bdbee4a6dccfe696203b96e408f66
mfreema1 <mfreema1@stevens.edu>
2018-09-28 11:43:01 -0400

    Fill out test cases

--- tests/test_us03.py
+++ tests/test_us03.py
@@ -1,33 +1,26 @@
 from modules.us03 import get_rows
-from parser import main
-from utilities import reset_db
+from utilities import execute_test
 import sqlite3
 
 conn = sqlite3.connect(':memory:')
+# conn = sqlite3.connect('megatron.db') #can also use megatron to inspect output
 
-#All good entries
-def test_us03_1():
-    reset_db(conn)
-    main('./ged/us03_01.ged', conn)
+def test_us03_01():
+    execute_test('./ged/us03_01.ged', conn)
     assert get_rows(conn) == []
 
-# #Single bad entry
-# def test_us03_2():
-#     assert get_rows('/home/mark/Desktop/school/fall2018/ssw-555/proj/GEDCOMProj/dbs/us03_2.db') == [('I11', '1960-01-01', '1950-01-01')]
+def test_us03_02():
+    execute_test('./ged/us03_02.ged', conn)
+    assert get_rows(conn) == [('US03_T02_I01', '2000-01-01', '1990-01-01')]
 
-# #Multi bad entry
-# def test_us03_3():
-#     assert get_rows('/home/mark/Desktop/school/fall2018/ssw-555/proj/GEDCOMProj/dbs/us03_3.db') == [('I11', '1960-01-01', '1950-01-01'),('I13', '1980-01-01', '1910-01-01')]
+def test_us03_03():
+    execute_test('./ged/us03_03.ged', conn)
+    assert get_rows(conn) == [('US03_T03_I01', '2000-01-01', '1990-01-01'), ('US03_T03_I02', '2000-01-01', '1990-01-01'), ('US03_T03_I03', '2000-01-01', '1990-01-01')]
 
-# #Multi bad entry with missing birthday
-# def test_us03_4():
-#     assert get_rows('/home/mark/Desktop/school/fall2018/ssw-555/proj/GEDCOMProj/dbs/us03_4.db') == [('I11', '1960-01-01', '1950-01-01'),('I13', '1980-01-01', '1910-01-01')]
+def test_us03_04():
+    execute_test('./ged/us03_04.ged', conn)
+    assert get_rows(conn) == []
 
-# #Never born or dead
-# def test_us03_5():
-#     assert get_rows('/home/mark/Desktop/school/fall2018/ssw-555/proj/GEDCOMProj/dbs/us03_5.db') == []
-
-# def test_us03_4():
-#     assert get_rows('../temp.db') == [] #we are allowed to go one level out, this gives me an idea...
-# perhaps a temporary testing db that we inflate and destroy as we test? This way we write GEDCOM involving the parser and only have
-# .ged testing files instead of db files
+def test_us03_05():
+    execute_test('./ged/us03_05.ged', conn)
+    assert get_rows(conn) == [('US03_T05_I02', '2000-01-01', '1990-01-01'), ('US03_T05_I04', '2000-01-01', '1990-01-01')]
