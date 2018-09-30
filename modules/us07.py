ec7f799b763138856bdb37ec378b84b5b7a5162f
mfreema1 <mfreema1@stevens.edu>
2018-09-29 20:59:11 -0400

    Start US07

--- modules/us07.py
+++ modules/us07.py
@@ -1,27 +1,21 @@
-import sqlite3
-import datetime
+"""
+User Story 07 ensures that no person be over 150 years of age.
 
-def get_rows(conn):
-    c = conn.cursor()
-    res = []
-    rows = c.execute("SELECT ID, Birthday, Death FROM INDI WHERE Age > 150 ").fetchall()
-    for row in rows:
-        #Check for if death occurred at least but too late
-        if row[2] != 'NA':
-            res.append(row)
-            res.append('Y')
-        else:
-            res.append(row)
-            res.append('N')
-    return res
-def US07(conn):
-    list = get_row(conn)
-    print_records(list)
+@author: Besnik Balaj
+"""
 
-def post_rows(list):
-    #list: 0-ID, 1-Birthday, 2-Death, 3-Check
-    for row in list:
-        if res[3] == 'Y':
-            print('ERROR: Death is greater than 150 years after Birth for ' + list[0])
-        else:
-            print('ERROR: Current date is not less than 150 years after Birth for' + list[0] )
+from user_story import UserStory
+
+class UserStory07(UserStory):
+
+    def print_rows(self, rows):
+        for row in rows:
+            if row[1] != "NA":
+                print('ERROR: Death is greater than 150 years after birth for ' + row[0])
+            else:
+                print('ERROR: Current date is not less than 150 years after birth for ' + row[0])
+
+    def get_rows(self, conn):
+        c = conn.cursor()
+        rows = c.execute('SELECT ID, Death FROM INDI WHERE Age > 150').fetchall()
+        return rows
