128803360b41f934e89a015fdbc9e57a26ee021d
mfreema1 <mfreema1@stevens.edu>
2018-10-01 22:45:37 -0400

    Update tests to new class-based syntax

--- tests/test_us02.py
+++ tests/test_us02.py
@@ -4,29 +4,29 @@ Test for user story 02.
 @author: Kipsy Quevada, pair programmed with Besnik Balaj
 """
 
-from modules.us02 import get_rows
+from modules.us02 import UserStory02
 from lib.utilities import execute_test
-import sqlite3
+from sqlite3 import connect
 
-conn = sqlite3.connect(':memory:')
-# conn = sqlite3.connect('megatron.db') #can also use megatron to inspect output
+conn = connect(':memory:')
+user_story_02 = UserStory02()
 
 def test_us02_01():
     execute_test('us02_01.ged', conn)
-    assert get_rows(conn) == []
+    assert user_story_02.get_rows(conn) == []
 
 def test_us02_02():
     execute_test('us02_02.ged', conn)
-    assert get_rows(conn) == [('US02_T02_I01', '3000-01-01', '2000-01-01')]
+    assert user_story_02.get_rows(conn) == [('US02_T02_I01', '3000-01-01', '2000-01-01')]
 
 def test_us02_03():
     execute_test('us02_03.ged', conn)
-    assert get_rows(conn) == []
+    assert user_story_02.get_rows(conn) == []
 
 def test_us02_04():
     execute_test('us02_04.ged', conn)
-    assert get_rows(conn) == [('US02_T04_I01', '2000-01-01', '1000-01-01'), ('US02_T04_I02', '2000-01-01', '1000-01-01'), ('US02_T04_I03', '2000-01-01', '1000-01-01')]
+    assert user_story_02.get_rows(conn) == [('US02_T04_I01', '2000-01-01', '1000-01-01'), ('US02_T04_I02', '2000-01-01', '1000-01-01'), ('US02_T04_I03', '2000-01-01', '1000-01-01')]
 
 def test_us02_05():
     execute_test('us02_05.ged', conn)
-    assert get_rows(conn) == [('US02_T05_I03', '2000-01-01', '1000-01-01'), ('US02_T05_I04', '2000-01-01', '1000-01-01')]
+    assert user_story_02.get_rows(conn) == [('US02_T05_I03', '2000-01-01', '1000-01-01'), ('US02_T05_I04', '2000-01-01', '1000-01-01')]
--- tests/test_us03.py
+++ tests/test_us03.py
@@ -4,29 +4,29 @@ Test for user story 03.
 @author: Mark Freeman
 """
 
-from modules.us03 import get_rows
+from modules.us03 import UserStory03
 from lib.utilities import execute_test
-import sqlite3
+from sqlite3 import connect
 
-conn = sqlite3.connect(':memory:')
-# conn = sqlite3.connect('megatron.db') #can also use megatron to inspect output
+conn = connect(':memory:')
+user_story_03 = UserStory03()
 
 def test_us03_01():
     execute_test('us03_01.ged', conn)
-    assert get_rows(conn) == []
+    assert user_story_03.get_rows(conn) == []
 
 def test_us03_02():
     execute_test('us03_02.ged', conn)
-    assert get_rows(conn) == [('US03_T02_I01', '2000-01-01', '1990-01-01')]
+    assert user_story_03.get_rows(conn) == [('US03_T02_I01', '2000-01-01', '1990-01-01')]
 
 def test_us03_03():
     execute_test('us03_03.ged', conn)
-    assert get_rows(conn) == [('US03_T03_I01', '2000-01-01', '1990-01-01'), ('US03_T03_I02', '2000-01-01', '1990-01-01'), ('US03_T03_I03', '2000-01-01', '1990-01-01')]
+    assert user_story_03.get_rows(conn) == [('US03_T03_I01', '2000-01-01', '1990-01-01'), ('US03_T03_I02', '2000-01-01', '1990-01-01'), ('US03_T03_I03', '2000-01-01', '1990-01-01')]
 
 def test_us03_04():
     execute_test('us03_04.ged', conn)
-    assert get_rows(conn) == []
+    assert user_story_03.get_rows(conn) == []
 
 def test_us03_05():
     execute_test('us03_05.ged', conn)
-    assert get_rows(conn) == [('US03_T05_I02', '2000-01-01', '1990-01-01'), ('US03_T05_I04', '2000-01-01', '1990-01-01')]
+    assert user_story_03.get_rows(conn) == [('US03_T05_I02', '2000-01-01', '1990-01-01'), ('US03_T05_I04', '2000-01-01', '1990-01-01')]
