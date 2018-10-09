00f7e8bce98c439487a4f5d922b6b4c13eded2b9
Besnik Balaj <nickbalaj@hotmail.com>
2018-10-08 22:28:07 -0400

    Add tests for US10

---
+++ tests/test_us10.py
@@ -0,0 +1,37 @@
+"""
+Test for User Story 10.
+
+@author: Besnik Balaj
+"""
+
+from modules.us10 import UserStory10
+from lib.utilities import execute_test
+from sqlite3 import connect
+
+conn = connect(':memory:')
+user_story_10 = UserStory10()
+
+#Wife was not 14 years of age
+def test_us10_01():
+    execute_test('us10_01.ged', conn)
+    assert user_story_10.get_rows(conn) == [['US34_T01_I01', '50', 'US34_T01_I02','10']]
+
+#2 wives were not of 14 years of age
+def test_us10_02():
+    execute_test('us10_02.ged', conn)
+    assert user_story_10.get_rows(conn) == [['US34_T02_I03', '50', 'US34_T02_I04','10'],['US34_T02_I01', '50', 'US34_T02_I02','10']]
+
+#Husband was not 14 years of Age
+def test_us10_03():
+    execute_test('us10_03.ged', conn)
+    assert user_story_10.get_rows(conn) == [['US34_T03_I01', '10', 'US34_T03_I02','50']]
+
+#2 Husbands were not of 14 years of age
+def test_us10_04():
+    execute_test('us10_04.ged', conn)
+    assert user_story_10.get_rows(conn) == [['US34_T04_I03', '10', 'US34_T04_I04','50'],['US34_T04_I01', '10', 'US34_T04_I02','50']]
+
+#Mix of husband and wife being not of age
+def test_us10_05():
+    execute_test('us10_05.ged', conn)
+    assert user_story_10.get_rows(conn) == [['US34_T05_I03', '9', 'US34_T05_I04','50'],['US34_T05_I01', '50', 'US34_T05_I02','9']]
