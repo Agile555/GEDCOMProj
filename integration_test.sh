3702c6dd99bde2919afbd7961aec2613b2732233
mfreema1 <mfreema1@stevens.edu>
2018-10-30 18:13:59 -0400

    Fix hunter being placed in optimus

--- integration_test.sh
+++ integration_test.sh
@@ -1,3 +1,3 @@
 rm -f ./ged/optimus_prime.ged
-for f in ./ged/*; do (cat "${f}"; echo) | grep '\S' >> ./ged/optimus_prime.ged; done
+for f in ./ged/*.ged; do (cat "${f}"; echo) | grep '\S' >> ./ged/optimus_prime.ged; done
 python index.py
