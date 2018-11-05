c8ed665e917b320b9d250ce7eaf318f32b2b2425
Kipsy Quevada <quekipz@gmail.com>
2018-11-05 16:30:09 -0500

    Fix rows array to print for US24

--- modules/us24.py
+++ modules/us24.py
@@ -19,9 +19,11 @@ class UserStory24(UserStory):
         c = conn.cursor()
         res = []
         family = []
+        rows = []
         family = c.execute('SELECT ID, Married, "Husband Name", "Wife Name" FROM FAM WHERE Married != "NA" AND "Husband Name" != "NA" AND "Wife Name" != "NA"').fetchall()
         for i, fam in enumerate(family):
             for fam2 in family[i+1:]:
                 if fam[1:] == fam2[1:]:
                     res.append((fam[0], fam2[0]))
+                    rows.append((fam[0], fam2[0]))
         return res
