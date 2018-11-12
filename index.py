32696deb5b1c42f9fe4acb1eaf5261e922a8eb5e
Besnik Balaj <nickbalaj@hotmail.com>
2018-11-11 20:15:24 -0500

    Update Index with User Stories

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ Entry point for GEDCOM parsing application.
 @author: Mark Freeman, Michael Ameer, Besnik Balaj, Kipsy Quevada
 """
 
-from modules import us01, us02, us03, us04, us05, us06, us07, us08, us09, us10, us12, us18, us27, us29, us30, us31, us32, us33, us34, us35, us36, us38, us39, us41
+from modules import us01, us02, us03, us04, us05, us06, us07, us08, us09, us10, us12, us13, us15, us16, us18, us21, us23, us24, us27, us28, us29, us30, us31, us32, us33, us34, us35, us36, us38, us39, us41
 from lib.utilities import reset_db, execute_test
 import sqlite3
 
@@ -29,8 +29,15 @@ def main():
     stories.append(us09.UserStory09())
     stories.append(us10.UserStory10())
     stories.append(us12.UserStory12())
+    stories.append(us13.UserStory13())
+    stories.append(us15.UserStory15())
+    stories.append(us16.UserStory16())
     stories.append(us18.UserStory18())
+    stories.append(us21.UserStory21())
+    stories.append(us23.UserStory23())
+    stories.append(us24.UserStory24())
     stories.append(us27.UserStory27())
+    stories.append(us28.UserStory28())
     stories.append(us29.UserStory29())
     stories.append(us30.UserStory30())
     stories.append(us31.UserStory31())
