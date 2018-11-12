18f1313b3b120af0d1c6e51bbe488d6635d988ee
mfreema1 <mfreema1@stevens.edu>
2018-11-11 20:52:23 -0500

    Fix faulty query

--- modules/us13.py
+++ modules/us13.py
@@ -11,7 +11,9 @@ class UserStory13(UserStory):
 
     def print_rows(self, rows):
         for row in rows:
-            print('CURIOSITY: Family {0} has strange sibling spacing between children {1} and {2} born on {3}, and {4}'.format(row))
+            print(row)
+            print(len(row))
+            print('CURIOSITY: FAMILY: US13: Family {} has strange sibling spacing between children {} and {} born on {}, and {}'.format(row[0], row[1], row[2], row[3], row[4]))
 
     def get_rows(self, conn):
         c = conn.cursor()
@@ -19,7 +21,7 @@ class UserStory13(UserStory):
 
         families = c.execute('SELECT ID FROM FAM WHERE Children != "NA"').fetchall() or []
         for family in families:
-            children = c.execute('SELECT INDI_ID, Birthday FROM CHLD JOIN INDI ON (INDI.ID = INDI_ID) WHERE Birthday != "NA"').fetchall() or []
+            children = c.execute('SELECT INDI_ID, Birthday FROM CHLD JOIN INDI ON (INDI.ID = INDI_ID) WHERE (Birthday != "NA" AND FAM_ID = "{}")'.format(family[0])).fetchall() or []
             if(len(children) > 1):
                 #we have no choice to go O(n^2), was thinking we could go O(n log n) by sorting and then comparing only adjacent values (one pass)
                 #but non-adjacent ones can also match in that case.  For example, if we had three of the same birthday, then we would get:
